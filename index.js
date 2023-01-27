c58f6c686a9569739563b4653223efbe1e9c0aca
dim4ik85 <dim4ik1985@yandex.ru>
2023-01-27 22:40:45 +0300

    fix

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const hiddenNumber = Math.floor(Math.random() * 100) + 1;
 const rl = readline.createInterface(process.stdin, process.stdout);
 const pathFile = path.join(__dirname, 'log');
 
+
+
 function compare (numUser, numHidden) {
     if (numUser > numHidden) {
         return 'меньше';
@@ -28,53 +30,47 @@ let appFile = function (path, logFile) {
 }
 
 console.log(hiddenNumber);
+
 let logFile = 'Игра началась!!!\n';
 appFile(pathFile, logFile);
-rl.question('Угадайте число от 1 до 100: ',
-    (userInput) => {
-        console.log(`Ваше число ${userInput}`);
-        let counter = 0;
-        counter++;
-        appFile(pathFile, userInput);
-        if (+userInput === hiddenNumber) {
-            logFile = `\nВы угадали число c первой попытки!!!`;
-            rl.close();
-            appFile(pathFile, logFile)
-            console.log(`Вы угадали число с первой попытки`);
-        } else if (userInput < 101 && userInput > 0 && !isNaN(+userInput)){
-            logFile = `\nНеверно! Загаданное число ${compare(userInput, hiddenNumber)}! Попытка номер ${counter}`;
-            counter++;
-            rl.setPrompt(`Неверно! Загаданное число ${compare(userInput, hiddenNumber)}! Попытка номер ${counter}: `);
-            rl.prompt();
-            appFile(pathFile, logFile);
-            rl.on('line', (userInput) => {
-                appFile(pathFile, `\n${userInput}`);
-                if (+userInput === hiddenNumber) {
-                    logFile = `\nВы угадали число!!! Количество попыток: ${counter}.`;
-                    appFile(pathFile, logFile);
-                    rl.close();
-                    console.log(`Вы угадали число!!! Количество попыток: ${counter}.`);
-                } else if (userInput < 101 && userInput > 0 && !isNaN(+userInput)) {
-                    counter++;
-                    rl.setPrompt(`Неверно! Загаданное число ${compare(userInput, hiddenNumber)}! Попытка номер ${counter} `);
-                    rl.prompt();
-                    logFile = `\nНеверно! Загаданное число ${compare(userInput, hiddenNumber)}! Попытка номер ${counter}`;
-                    appFile(pathFile, logFile);
-                } else {
-                    logFile = '\nНеверный ввод!!!';
-                    appFile(pathFile, logFile);
-                    console.log('Неверный ввод2!!!');
-                    rl.close();
-                }
-            })
-        } else {
-            logFile = '\nНеверный ввод!!!'
-            appFile(pathFile, logFile);
-            console.log('Неверный ввод1!!!');
-            rl.close();
-        }
-    })
 
+let counter = 0;
+function question() {
+
+    rl.question('Enter number: ', (inputUser) => {
+        logFile = `${inputUser}\n`
+        appFile(pathFile, logFile);
+        if (inputUser === 'q') {
+            rl.close();
+            return
+        }
+
+        if (+inputUser === hiddenNumber) {
+            counter++;
+            logFile = `Вы угадали число! Попыток: ${counter}`;
+            appFile(pathFile, logFile);
+            rl.close();
+            return
+        }
+
+        if ((inputUser < 101 && inputUser > 0 && !isNaN(+inputUser))) {
+            counter++;
+            logFile = `Загаданное число ${compare(inputUser, hiddenNumber)}. Попытка: ${counter + 1}\n`;
+            appFile(pathFile, logFile);
+            question();
+            return
+        } else {
+            console.log('Недопустимый ввод');
+            rl.close();
+            return;
+        }
+
+        counter++;
+        question();
+    })
+}
+
+question();
 
 
 
